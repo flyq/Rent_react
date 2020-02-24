82f3333948c0de25ed98cbce08e0848198096829
flyq <flyq951@gmail.com>
2020-02-23 16:15:18 -0800

    add

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
     chain: 'https://cita-test.ethgeek.cn:8888',
     privateKey: '0xcba2a9c5b037ba844e695d019ef6c596e9af150d8ad03567d42b9d977e0e\
 41c1',
-    contractAddress: '0x3b6c6C8022c858bA316122A873A4EF974754D247'
+    contractAddress: '0x35C610DA2dC04bd94990da0D86D0329d96e16410'
 }
 module.exports = config
 
